b54267aab5770c717e314b1343c74c565a5e4878
mkucukos <94495145+mkucukos@users.noreply.github.com>
2021-12-07 23:30:52 -0800

    deleted redundant info

--- btc_viewer.py
+++ btc_viewer.py
@@ -1,7 +1,7 @@
 # import pycoingecko
 from pycoingecko import CoinGeckoAPI 
 
-# import pandas and numpy
+# import pandas, numpy and matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
